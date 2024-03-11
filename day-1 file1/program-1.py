4450fa51af64d7a98d6861254cb5934f26edac24
smd.sahil <161965940+sahil786786786@users.noreply.github.com>
2024-03-11 15:59:46 +0530

    Add files via upload

---
+++ day-1 file1/program-1.py
@@ -0,0 +1,32 @@
+"""
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello world")
+----> commenting
+1.)commenting is used for making documentation in a python file
+2.)when the line is commented,that line will not be considered as a part of the code.
+3.)when the program is executed, commented line will not be in the part of execution.
+
+----> NOTE:
+single line commenting can be done using # at the begining of the line
+    Eg:
+        #print ("hello world")
+multiple line commenting can be done using.capitalize the line between the quotes will be commented
+    Eg:
+    print ("hello world")
+    print ("hello world")
+    print ("hello world")
+    print ("hello world")
+"""
+import this
+
+
+# import is a keyword which is used to communicated between the python files.
+
+import antigravity
+
+
---
+++ day-1 file1/theroy (day-1).py
@@ -0,0 +1,55 @@
+''''
+---->DAY-1,Bargained characters
+1.)control and data structures- ABC language
+2.)strings and its formats- from c
+3.)Regular expressions- pearl
+4.)Exception handling- Module 3
+5.)Magic methods from- fortron
+
+---->what kind of language is python
+1.)object oriented programing language
+2.)dynamically typed language
+3.)interpreted language
+4.)compiled language
+
+---->Flavours of Python
+1.)Cpython- written in C,Most common Implementation of python
+2.)Jython- java implementation of python.
+3.)ironPython- implementation of the python programming language
+which is tightly integrated with .NET.
+4.)Brython- Browser Python,runs in browers
+5.)RubyPython- Bridge between Python and Ruby Interpreters
+6.)Micropython- Runs in microcontrollers.
+
+---->print,parameters
+print(object,sep,end,file,flush)
+print has 5 parameters
+
+object -->the value to be printed is object
+sep--> seperator
+Eg
+a = 8
+b = 9
+c = 89
+print(a,b,c sep='$')
+
+end -->which is used to add the string at the end of the value,
+#It will not keep the command to stay in same line
+Eg:
+print(str(a),b,end='%%')
+print(str(c))
+
+''''
+"""
+!--->Varibles
+?variables is a name which is used to identify the value of variable
+?which is used stored in a specific memory location
+
+"""
+
+"""
+! Rules to dedfine variables
+1.)Variables have to start with only alphabets,Undercores
+2.)The char following the 1st char can be alphabets,numbers,undercores
+3.)No spaces are allowed
+4.)No special
