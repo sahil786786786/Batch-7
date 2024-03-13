04a69ad436c4a3712c8f3945694454c1481aeca8
smd.sahil <161965940+sahil786786786@users.noreply.github.com>
2024-03-13 16:17:37 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,206 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py ===============
+enter the length15
+enter the breadth15
+it is a square
+>>> 
+=============== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py ===============
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 35
+yes
+>>> 
+=============== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py ===============
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 25
+no
+>>> 
+=============== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py ===============
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 35
+yes
+enter the age of the person: 45
+enter the age of the person: 55
+enter the age of the person: 65
+enter the age of the person: 25
+error
+>>> 
+=============== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py ===============
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 35
+yes
+enter the age of the person: 55
+enter the age of the person: 45
+enter the age of the person: 35
+enter the age of the person: 25
+error
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the age of the person: 85
+enter the age of the person: 45
+enter the age of the person: 35
+enter the age of the person: 25
+error
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 65
+enter the age of the person: 55
+enter the age of the person: 45
+enter the age of the person: 35
+error
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 85
+enter the age of the person: 45
+enter the age of the person: 25
+enter the age of the person: 16
+error
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length15
+enter the breadth15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 85
+enter the age of the person: 45
+enter the age of the person: 25
+enter the age of the person: 16
+error
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 45
+enter the breadth= 45
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 85
+enter the age of the person: 45
+enter the age of the person: 12
+enter the age of the person: 11
+error
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 85
+enter the age of the person: 45
+enter the age of the person: 12
+enter the age of the person: 16
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 45
+enter the age of the person: 45
+enter the age of the person: 45
+enter the age of the person: 45
+Enter the price: 1000000
+the onroad cost of bike is:  1081000.0
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 45
+no
+enter the age of the person: 45
+enter the age of the person: 55
+enter the age of the person: 78
+C is greater
+Enter the price: 100000
+the onroad cost of bike is:  105000.0
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 80
+enter the age of the person: 45
+enter the age of the person: 56
+enter the age of the person: 65
+A is greater
+Enter the price: 1500000
+the onroad cost of bike is:  1621500.0
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 45
+enter the age of the person: 12
+enter the age of the person: 11
+enter the age of the person: 10
+A is greater
+Enter the price: 1000000
+the onroad cost of bike is:  1081000.0
+enter the marks45
+F,FAil
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 45
+enter the age of the person: 15
+enter the age of the person: 12
+enter the age of the person: 11
+A is greater
+Enter the price: 1000000
+the onroad cost of bike is:  1081000.0
+enter the marks45
+D
+>>> 
+========================================== RESTART: E:/219Y1A0247/day-3 file-3/program-3.py =========================================
+enter the length= 15
+enter the breadth= 15
+it is a square
+enter the number: 70
+yes
+enter the age of the person: 45
+enter the age of the person: 45
+enter the age of the person: 15
+enter the age of the person: 55
+D is greater
+Enter the price: 1000000
+the onroad cost of bike is:  1081000.0
+enter the marks55
+C
+>>> 
